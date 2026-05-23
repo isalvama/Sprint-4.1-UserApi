40744807a709409ee3a44fc456bf5c95441c5fa1
isalvama <inesalvans@gmail.com>
2026-05-23 17:06:04 +0200

    fix: add "/search/" after "/users" and before "{name}" in getUserByName URL to prevent ambiguation with getUserById URL

--- src/main/java/cat/itacademy/s04/t01/userapi/user/UserController.java
+++ src/main/java/cat/itacademy/s04/t01/userapi/user/UserController.java
@@ -34,21 +34,8 @@ public class UserController {
         return users.stream().filter(user -> user.getUuid().toString().equals(id)).findFirst().orElseThrow(() -> new UserNotFoundException("Could not find the user with id " + id));
     }
 
-    @GetMapping("/users/{name}")
+    @GetMapping("/users/search/{name}")
     public User getUserByName(@PathVariable String name) {
         return users.stream().filter(user -> user.getName().equalsIgnoreCase(name)).findFirst().orElseThrow(() -> new UserNotFoundException("Could not find the user with name " + name));
     }
 }
-
-/*
-
-Els tests que farem són de tipus test de controladors (o tests de capa web). No necessitem una base de dades ni serveis externs: només provarem que les rutes (endpoints) responen correctament davant diferents peticions.
-
-Objectius del test
-Assegurar que GET /users retorna una llista correcta.
-Verificar que POST /users afegeix un usuari i retorna el resultat amb el seu UUID.
-Comprovar que GET /users/{id} retorna l’usuari correcte si existeix.
-Retornar error 404 si es demana un id que no existeix.
-Validar que el filtre per nom GET /users?name= funciona com cal.
-
- */

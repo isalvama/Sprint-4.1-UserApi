3a57638dce6dd49329bd72419d2dd68e131e4c96
isalvama <inesalvans@gmail.com>
2026-05-26 17:16:36 +0200

    fix: correct typos in message parameter of UserAlreadyExistsException constructor

--- src/main/java/cat/itacademy/s04/t01/userapi/user/exception/UserAlreadyExistsException.java
+++ src/main/java/cat/itacademy/s04/t01/userapi/user/exception/UserAlreadyExistsException.java
@@ -2,6 +2,6 @@ package cat.itacademy.s04.t01.userapi.user.exception;
 
 public class UserAlreadyExistsException extends RuntimeException {
     public UserAlreadyExistsException(String email) {
-        super("User with already exists with email: " + email);
+        super("User already exists with email: " + email);
     }
 }

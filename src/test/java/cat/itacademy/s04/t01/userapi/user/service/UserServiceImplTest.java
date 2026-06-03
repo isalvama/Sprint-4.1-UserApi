b3c8c679f11536631faee9bb39daf9ebdc531d44
isalvama <inesalvans@gmail.com>
2026-06-03 11:28:25 +0200

    test: add verifications of interactions with the repository in tests of UserServiceImplTest to validate performance and bussiness flow

--- src/test/java/cat/itacademy/s04/t01/userapi/user/service/UserServiceImplTest.java
+++ src/test/java/cat/itacademy/s04/t01/userapi/user/service/UserServiceImplTest.java
@@ -23,8 +23,7 @@ import java.util.UUID;
 
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.*;
 
 class UserServiceImplTest {
 
@@ -48,7 +47,7 @@ class UserServiceImplTest {
     @DisplayName("createUser()")
     class createUser{
         @Test
-        @DisplayName("createUser() creates a new User successfully returning UserResponse with the user's data and generated UUID id")
+        @DisplayName("createUser() creates a new User successfully returning UserResponse with the user's data and a generated UUID id")
         void createsUser_whenUserWithMatchingEmailDoesNotExist() {
             when(userRepository.existsByEmail(EMAIL)).thenReturn(false);
             when(userRepository.save(any(User.class))).thenAnswer(AdditionalAnswers.returnsFirstArg());
@@ -59,6 +58,9 @@ class UserServiceImplTest {
             assertNotNull(response.id());
             assertEquals(NAME, response.name());
             assertEquals(EMAIL, response.email());
+
+            verify(userRepository, times(1)).existsByEmail(EMAIL);
+            verify(userRepository, times(1)).save(any());
         }
 
         @Test
@@ -68,6 +70,9 @@ class UserServiceImplTest {
 
             Exception exception = assertThrows(UserAlreadyExistsException.class, ()-> {userService.createUser(createUserDto);});
             assertEquals("User already exists with email: " + EMAIL, exception.getMessage());
+
+            verify(userRepository, times(1)).existsByEmail(EMAIL);
+            verify(userRepository, never()).save(any(User.class));
         }
     }
 
@@ -86,6 +91,8 @@ class UserServiceImplTest {
             assertEquals(NAME, response.getFirst().name());
             assertEquals(EMAIL, response.getFirst().email());
             assertEquals(ID.toString(), response.getFirst().id());
+
+            verify(userRepository, times(1)).findAll();
         }
 
         @Test
@@ -97,6 +104,8 @@ class UserServiceImplTest {
 
             assertNotNull(response);
             assertEquals(0, response.size());
+
+            verify(userRepository, times(1)).findAll();
         }
     }
 
@@ -114,15 +123,19 @@ class UserServiceImplTest {
             assertEquals(NAME, response.name());
             assertEquals(EMAIL, response.email());
             assertEquals(ID.toString(), response.id());
+
+            verify(userRepository, times(1)).findById(ID);
         }
 
         @Test
         @DisplayName("getUserById() throws UserNotFoundException when a user with matching id does not exist in db")
-        void shouldThrowUserNotFoundException_whenUserDoNotExistInDbWithMatchingId() {
+        void shouldThrowUserNotFoundException_whenUserDoesNotExistInDbWithMatchingId() {
             when(userRepository.findById(any())).thenReturn(Optional.empty());
 
             Exception exception = assertThrows(UserNotFoundException.class, ()->{userService.getUserById(String.valueOf(ID));});
             assertEquals("User not found with id: " + ID, exception.getMessage());
+
+            verify(userRepository, times(1)).findById(ID);
         }
     }
 
@@ -130,7 +143,7 @@ class UserServiceImplTest {
     @DisplayName("getUserByName()")
     class getUserByName{
         @Test
-        @DisplayName("getUserByName() returns a List<UserResponse> instance with users data when users with the name passed as parameter exist in db")
+        @DisplayName("getUserByName() returns a List<UserResponse> instance with multiple user's data when users with the name passed as parameter exist in db")
         void shouldReturnListOfUserResponse_whenUsersWithNameExistInDb() {
             when(userRepository.searchByName(NAME)).thenReturn(List.of(user));
 
@@ -140,15 +153,19 @@ class UserServiceImplTest {
             assertEquals(NAME, response.getFirst().name());
             assertEquals(EMAIL, response.getFirst().email());
             assertEquals(ID.toString(), response.getFirst().id());
+
+            verify(userRepository, times(1)).searchByName(NAME);
         }
 
         @Test
-        @DisplayName("getUserById() throws UserNotFoundException when a user with matching id does not exist in db")
+        @DisplayName("getUserByName() throws UserNotFoundException when a user with matching id does not exist in db")
         void shouldThrowUserNotFoundException_whenUsersWithNameDoNotExistsInDb() {
             when(userRepository.searchByName(any())).thenReturn(List.of());
 
             Exception exception = assertThrows(UserNotFoundException.class, ()->{userService.getUserByName(NAME);});
             assertEquals("User not found with name: " + NAME, exception.getMessage());
+
+            verify(userRepository, times(1)).searchByName(NAME);
         }
     }
 }

dd781fb64259549424833b84e3f889f062e2edce
isalvama <inesalvans@gmail.com>
2026-06-03 11:30:54 +0200

    refactor: add necessary spring-boot-starter-test dependecy to make tests work in pom.xml and remove unused imports in CreateUserDto

--- src/main/java/cat/itacademy/s04/t01/userapi/user/dto/CreateUserDto.java
+++ src/main/java/cat/itacademy/s04/t01/userapi/user/dto/CreateUserDto.java
@@ -1,8 +1,6 @@
 package cat.itacademy.s04.t01.userapi.user.dto;
 
-
 import jakarta.validation.constraints.*;
-import org.jetbrains.annotations.NotNull;
 
 public record CreateUserDto(
         @NotBlank(message = "Name cannot be empty")
